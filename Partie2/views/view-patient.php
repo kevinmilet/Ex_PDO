b2f18ad17a58ebd9eab72c94c2b25fcfc210c3d1
kevinmilet <kevin.milet@gmail.com>
2021-02-07 09:16:10 +0100

    ...

--- Partie2/views/view-patient.php
+++ Partie2/views/view-patient.php
@@ -5,11 +5,17 @@
         <div class="col-md-6">
             <form action="" method="POST">
                 <?php foreach ($patient as $data): ?>
-                    <input class="form-control mb-3" type="text" value="<?=htmlentities($data->lastname)?>" name="lastname">
-                    <input class="form-control mb-3" type="text" value="<?=htmlentities($data->firstname)?>" name="firstname"> 
-                    <input class="form-control mb-3" type="date" value="<?=htmlentities($data->birthdate)?>" name="birthdate">
-                    <input class="form-control mb-3" type="text" value="<?=htmlentities($data->phone)?>" minlentght="10" maxlength="10" name="phone">
-                    <input class="form-control mb-3" type="email" value="<?=htmlentities($data->mail)?>" name="mail">
+                    <p><strong>Nom: </strong><?=htmlentities($data->lastname)?></p>
+                    <input class="form-control mb-3" type="text" name="lastname">
+                    <p><strong>Prénom: </strong><?=htmlentities($data->firstname)?></p>
+                    <input class="form-control mb-3" type="text" name="firstname">
+                    <p><strong>Date de naissance: </strong><?=htmlentities($data->birthdate)?></p>
+                    <input class="form-control mb-3" type="date" name="birthdate">
+                    <p><strong>Téléphone: </strong><?=htmlentities($data->phone)?></p>
+                    <input class="form-control mb-3" type="text" minlentght="10" maxlength="10" name="phone">
+                    <p><strong>Email: </strong><?=htmlentities($data->mail)?></p>
+                    <input class="form-control mb-3" type="email" name="mail">
+                    <p>Remplissez les champs à modifier</p>
                 <?php endforeach ?>
                 <div class="m-3">
                     <a href="liste-patient.php" type="button" class="btn btn-secondary">Annuler</a>
